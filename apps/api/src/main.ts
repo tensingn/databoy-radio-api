2c172f740bcc060890304c05b92749b69d5a0a7c
tensingn <tensingn@oregonstate.edu>
2022-03-17 19:53:55 -0400

    updating package.json scripts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { env } from 'process';
 import { AppModule } from './app.module';
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
+  app.enableCors();
 
   if (module.hot) {
     module.hot.accept();
