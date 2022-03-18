d4a5f5362c373875d48e8cf07173efa75ef253f6
tensingn <tensingn@oregonstate.edu>
2022-03-17 20:07:06 -0400

    removing cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,7 +6,6 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000);
-  app.enableCors();
 
   if (module.hot) {
     module.hot.accept();
