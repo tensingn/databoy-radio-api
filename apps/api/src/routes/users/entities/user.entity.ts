f42f1c593f321bd9e2c6e708794802e7b430f01a
tensingn <tensingn@oregonstate.edu>
2023-10-23 18:10:49 -0400

    some adjustments to firestore service and to users service

--- apps/api/src/routes/users/entities/user.entity.ts
+++ apps/api/src/routes/users/entities/user.entity.ts
@@ -1,4 +1,6 @@
-export class User {
+import { DatabaseObject } from 'apps/api/src/services/database/models/database-object.entity';
+
+export class User extends DatabaseObject {
   userID: string;
   username: string;
   email: string;
